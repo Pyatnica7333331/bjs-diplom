ed1989f7cfbbcdba3bc33218db246cb4e300ead0
Pyatnica7333331 <alferova7333331@gmail.com>
2021-06-01 23:37:21 +0300

    nothing good

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,4 @@
-const exit = new LogoutButton;
+const exit = new LogoutButton();
 exit.action = () => {
     ApiConnector.logout(response => {
         if(response.success) {
@@ -12,69 +12,85 @@ ApiConnector.current(response => {
         ProfileWidget.showProfile(response.data)
     };
 });  
-const ratesBoard = new RatesBoard;
-const fill = ApiConnector.getStocks (response => {
+const ratesBoard = new RatesBoard();
+const fill = () => ApiConnector.getStocks (response => {
     if(response.success) {
         ratesBoard.clearTable();
         ratesBoard.fillTable(response.data);
     };
 });
-
 setInterval(() => {
-    fill;
+    fill();
 }, 60000); 
 
-const moneyManager = new MoneyManager;
+const moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = (data) => {
         ApiConnector.addMoney(data, response => {
         if(response.success) {
-        ProfileWidget.showProfile(response.data)
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Действие прошло успешно" || console.error())
     };
     
-    //moneyManager.setMessage(moneyManager.errorMessageBlock))
+    
     });
     
 };
 moneyManager.conversionMoneyCallback = (data) => {
     ApiConnector.convertMoney(data, response => {
         if(response.success) {
-            ProfileWidget.showProfile(response.data)
-        }
+            ProfileWidget.showProfile(response.data);
+            
+        };
+        try {
+            moneyManager.setMessage(response.success, moneyManager.errorMessageBlock || "Действие прошло успешно");
+        } catch (err) {
+            moneyManager.setMessage(err, moneyManager.errorMessageBlock)
+        };
     })
 };
 moneyManager.sendMoneyCallback = (data) => {
     ApiConnector.transferMoney(data, response => {
         if(response.success) {
-            ProfileWidget.showProfile(response.data)
-        }
+            ProfileWidget.showProfile(response.data);
+        };
+        try {
+            moneyManager.setMessage(response.success, err.message ||"Действие прошло успешно");
+        } catch (err) {
+            moneyManager.setMessage(err, err.message)
+        };
     })
 };
-const favWidget = new FavoritesWidget;
+const favWidget = new FavoritesWidget();
 ApiConnector.getFavorites ( response => {
     if(response.success) {
+        
         favWidget.clearTable();
         favWidget.fillTable(favWidget.favoritesTableBody);
         moneyManager.updateUsersList(favWidget.favoritesTableBody);
-        //favWidget.setMessage(isSuccess, message);
     };
+    
 });
 favWidget.addUserCallback = (data) => {
     ApiConnector.addUserToFavorites (data, response => {
         if(response.success) {
             favWidget.clearTable();
+            favWidget.favoritesTableBody.push(favWidget.getData(favWidget.addUserToFavoritesForm ));
             favWidget.fillTable(favWidget.favoritesTableBody);
             moneyManager.updateUsersList(favWidget.favoritesTableBody);
-            //favWidget.setMessage(isSuccess, message);
         };
-    })
-};
+        try {
+            favWidget.setMessage(response.success, "Действие прошло успешно");
+        } catch (err) {
+            console.log(err.message)
+        };
+});
 favWidget.removeUserCallback = (data) => {
     ApiConnector.removeUserFromFavorites (data, response => {
         if(response.success) {
             favWidget.clearTable();
             favWidget.fillTable(favWidget.favoritesTableBody);
             moneyManager.updateUsersList(favWidget.favoritesTableBody);
-            //favWidget.setMessage(isSuccess, message);
         };
+        favWidget.setMessage(response.success, "Действие прошло успешно");
     })
-};
+}};
